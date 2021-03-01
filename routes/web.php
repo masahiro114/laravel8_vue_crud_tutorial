d3b8f37528027f984af1699391289cafb111ebb5
Masahiro Yasuda <masahiro.y114@live.com>
2021-03-02 09:46:04 +1000

    updated web.php

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,15 @@ Route::get('{any}', function () {
     return view('app');
 })->where('any', '.*');
 
+
+Route::get('/api/posts', [PostController::class, 'index']);
+Route::post('/api/post/add', [PostController::class, 'add']);
+
+Route::get('/api/post/edit/{id}', [PostController::class, 'edit']);
+Route::post('/api/post/update/{id}', [PostController::class, 'update']);
+Route::post('/api/post/delete/{id}', [PostController::class, 'delete']);
+
+
 // Route::get('/api/posts', [App\Http\Controllers\API\PostController::class, 'index']);
 // Route::post('/api/post/add', [App\Http\Controllers\API\PostController::class, 'add']);
 
@@ -28,9 +37,9 @@ Route::get('{any}', function () {
 // Route::post('/api/post/update/{id}', [App\Http\Controllers\API\PostController::class, 'update']);
 // Route::post('/api/post/delete/{id}', [App\Http\Controllers\API\PostController::class, 'delete']);
 
-Route::get('/api/posts', 'PostController@index');
-Route::post('/api/post/add', 'PostController@add');
+// Route::get('/api/posts', 'API\PostController@index');
+// Route::post('/api/post/add', 'API\PostController@add');
 
-Route::get('/api/post/edit/{id}', 'PostController@edit');
-Route::post('/api/post/update/{id}', 'PostController@update');
-Route::post('/api/post/delete/{id}', 'PostController@delete');
+// Route::get('/api/post/edit/{id}', 'API\PostController@edit');
+// Route::post('/api/post/update/{id}', 'API\PostController@update');
+// Route::post('/api/post/delete/{id}', 'API\PostController@delete');
